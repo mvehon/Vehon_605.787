ba080aef40032e12c355900c066c723389229671
Matthew Vehon <mvehon@asu.edu>
2022-09-17 15:42:58 -0700

    Module 6 min requirements

---
+++ module6-solution/app.js
@@ -0,0 +1,27 @@
+(function () {
+    'use-strict';
+
+    angular.module('LunchCheck', [])
+
+        .controller('LunchCheckController', LunchCheckController)
+
+    LunchCheckController.$inject = ['$scope', '$filter']
+
+    function LunchCheckController($scope) {
+        $scope.itemString = ""
+
+        $scope.checkTooMuch = function () {
+            const items = $scope.itemString
+                .split(",")
+
+            if (!$scope.itemString.length) {
+                $scope.message = "Please enter data first"
+            } else if (items.length <= 3) {
+                $scope.message = "Enjoy!"
+            } else if (items.length > 3) {
+                $scope.message = "Too much!"
+            }
+        }
+    }
+})
+();
